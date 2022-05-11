13784f4c130de35499a06d296b02973c208a46dc
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-05-11 14:47:47 -0500

    coded out post reply

--- backend/reply/views.py
+++ backend/reply/views.py
@@ -1,12 +1,10 @@
-from xml.dom.minidom import Element
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from .models import Reply
 from .serializer import ReplySerializer
-from backend.reply import serializer
 
 # Create your views here.
 
@@ -20,4 +18,8 @@ def get_replies(request, pk):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def post_reply(request, pk):
-    
+    serializer = ReplySerializer(data=request.data, comment=pk )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
