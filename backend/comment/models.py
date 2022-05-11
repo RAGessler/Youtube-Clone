d1138767d2056cae4f1a138e4e31e87b2ca473c8
alex-colvin <alexcolvin@live.com>
2022-05-11 14:22:51 -0400

    debugged models, urls, views, models, to test server

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from authentication.models import User
+
 # Create your models here.
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
