c1a0262780ad75e6fe03678e03a7e03ee95ccee4
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-05-11 11:51:53 -0500

    finished comment and reply models and imports

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from authentication.models import User
+from comment.models import Comment
 
 # Create your models here.
 
 class Reply(models.Model):
-    user = models.ForeignKey()
-    comment = models.ForeignKey()
+    user = models.ForeignKey(User)
+    comment = models.ForeignKey(Comment)
     text = models.CharField()
