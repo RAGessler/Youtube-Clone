5c13b16be112b28ce14bfa90d351511635e88c5c
alex-colvin <alexcolvin@live.com>
2022-05-20 09:59:03 -0400

    video player tweak

--- frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
@@ -6,7 +6,7 @@ const VideoPlayer = (props)=>{
 
     return(
     <div className='video-player'>
-        <iframe style={{height:"75vh", width:"100vw"}} classname="iframe" src={`https://www.youtube.com/embed/${props.videoId}`} frameBorder="0"></iframe>
+        <iframe style={{height:"60vh", width:"60vw"}} classname="iframe" src={`https://www.youtube.com/embed/${props.videoId}`} frameBorder="0"></iframe>
     </div>
     
         )
